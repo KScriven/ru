785ba332798cd880fe7250da90ca0644418d1406
KScriven <kerryn.scriven@bbc.co.uk>
2017-07-23 15:42:01 +0100

    update to index.js to include epigram

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const url = process.env.SLACK_WEBHOOK_URL || '';
 const slackChannel = process.env.SLACK_CHANNEL || '';
 
 const webhook = new incomingWebhook(url);
-
-webhook.send('Hello human', function(err, res) {
+const epigramsOfTheDay = (eps[Math.floor(Math.random()*eps.length)]);
+      
+webhook.send(epigramsOfTheDay, function(err, res) {
     if (err) {
         console.log('Error:', err);
     } else {
         console.log('Message sent:', res);
     }
 });
-
-//console.log(eps[Math.floor(Math.random()*eps.length)]);
