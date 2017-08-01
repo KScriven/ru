700784fa316855a5144a7c9a2058a3eda6ac069d
KScriven <kerryn.scriven@bbc.co.uk>
2017-08-01 13:26:33 +0100

    update to slash config

--- src/clients/slash/slash.js
+++ src/clients/slash/slash.js
@@ -1,59 +1,6 @@
 'use strict';
 
-/*
-
-
-  To encrypt your secrets use the following steps:
-
-  1. Create or use an existing KMS Key - http://docs.aws.amazon.com/kms/latest/developerguide/create-keys.html
-
-  2. Click the "Enable Encryption Helpers" checkbox
-
-  3. Paste <COMMAND_TOKEN> into the kmsEncryptedToken environment variable and click encrypt
-
-Follow these steps to complete the configuration of your command API endpoint
-
-  1. When completing the blueprint configuration select "Open" for security
-     on the "Configure triggers" page.
-
-  2. Enter a name for your execution role in the "Role name" field.
-     Your function's execution role needs kms:Decrypt permissions. We have
-     pre-selected the "KMS decryption permissions" policy template that will
-     automatically add these permissions.
-
-  3. Update the URL for your Slack slash command with the invocation URL for the
-     created API resource in the prod stage.
-*/
-
-const AWS = require('aws-sdk');
-const qs = require('querystring');
-
-const slackToken = process.env.SLACK_VERIFICATION_TOKEN;
-let token;
-
-
-function processEvent(event, callback) {
-    const params = qs.parse(event.body);
-    const requestToken = params.token;
-    if (requestToken !== token) {
-        console.error(`Request token (${requestToken}) does not match expected`);
-        return callback('Invalid request token');
-    }
-
-    const user = params.user_name;
-    const command = params.command;
-    const channel = params.channel_name;
-    const commandText = params.text;
-
-    callback(null, `${user} invoked ${command} in ${channel} with the following text: ${commandText}`);
-}
-
-
-exports.handler = (event, context, callback) => {
-    if (slackToken) {
-        processEvent(event, context);
-        context.succeed();
-    } else {
-        callback('An error has occured');
-    }
+exports.handler = function(event, context) {
+    //Echo back the text the user typed in
+    context.succeed('You sent: ' + event.text);
 };
